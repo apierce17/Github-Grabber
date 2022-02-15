ea56d9be9ad92b552cad9d73ddc5f3acdf524411
Ashton Pierce <coalpierce@gmail.com>
2022-02-14 21:12:43 -0600

    Basic company search function built

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,32 @@
 $(document).ready( function() { 
+    // Delete Later
+    $('#companyName').val('Netflix');
 
-    $.ajax({
-        dataType: "json",
-        url: 'https://api.github.com/orgs/Netflix/repos',
-        success: function(repo) { 
-            console.log(repo);
+    var searchedCompany = {};
+    var searchedCompanyRepos = {};
+
+
+    $('#searchCompany').on('click', function(event) {
+        event.preventDefault();
+        if($('#companyName').val().trim().length === 0) {
+            // Throw error if field empty
+            console.log('sadad')
+            return
+        } else {
+            // Save searched company for later use
+            searchedCompany.searchedCompany = $("#companyName").val();
+            console.log(searchedCompany.searchedCompany);
+
+            // Get Company Info
+        $.ajax({
+            dataType: "json",
+            url: 'https://api.github.com/orgs/' + searchedCompany.searchedCompany + '/repos',
+            success: function(getCompany) { 
+                searchedCompany.companyAvatar = getCompany[0].owner.avatar_url;
+                searchedCompany.companyName = getCompany[0].owner.login;
+                console.log(getCompany);
+            }});
         }});
 
-
+    
 });
