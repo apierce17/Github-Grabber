b5eb352d1e43f9a6b35b55ce9a9d9b17eb969a9f
Ashton Pierce <coalpierce@gmail.com>
2022-02-17 23:43:54 -0600

    finished search functionality and desktop styling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready( function() {
 
     $('#search-empty').hide();
     $('#search-failed').hide();
+    $('.repo-commits').hide();
+    $('#current-company').hide();
+    
 
     var searchedCompany = {};
 
@@ -22,11 +25,18 @@ $(document).ready( function() {
     $('#companySearch').submit( function(event) {
         event.preventDefault();
         $('#repositories').empty();
+        $('#page-tabs').empty();
+        $('#current-company').empty();
         $('#search-empty').hide();
         $('#search-failed').hide();
+        $('#current-company').hide();
         if($('#companyName').val().trim().length === 0) {
             // Show error if field empty
             $('#search-empty').show();
+            $('.search').addClass('search-error');
+            setTimeout(function () { 
+                $('.search').removeClass('search-error');
+            }, 200);
             return
         } else {
             // Save searched company for later use
@@ -36,11 +46,17 @@ $(document).ready( function() {
             // Get Company Info
             $.ajax({
                 dataType: "json",
-                url: 'https://api.github.com/orgs/' + searchedCompany.searchedCompany + '/repos?per_page=50',
+                url: 'https://api.github.com/orgs/' + searchedCompany.searchedCompany + '/repos?per_page=100&page=2',
                 success: function(getCompanyRepositories) { 
                     searchedCompany.companyAvatar = getCompanyRepositories[0].owner.avatar_url;
                     searchedCompany.companyName = getCompanyRepositories[0].owner.login;
+                    searchedCompany.companyLink = getCompanyRepositories[0].owner.html_url;
 
+                    $('#current-company').append(
+                        '<img src="' + searchedCompany.companyAvatar + '" alt="profile picture">' +
+                        '<h1>' + searchedCompany.companyName + '</h1>'+ 
+                        '<a aria-label href="' + searchedCompany.companyLink + '" target="_blank"><i class="fa-brands fa-github-square"></i></a>');
+                    $('#current-company').show();
                     // Start pulling repositories 
                     $.each(getCompanyRepositories, function(i,e) {
                         var self = this;
@@ -54,6 +70,9 @@ $(document).ready( function() {
                         
                         // Only return created date
                         let repoDate = repoCreation.slice(0, 10);
+
+                        // Append company card
+                        
 
                         // Append each repository in it's own card
                         $('#repositories').append(
@@ -70,20 +89,24 @@ $(document).ready( function() {
                             '                <div class="forks"><i class="fa-solid fa-code-fork"></i></i> <span>' + repoForkCount + '</span></div>' +
                             '                <div class="languages"><span><i class="fa-solid fa-circle"></i> ' + repoLanguage + '</span>' + '</div>' +
                             '            </div>' +
+                            '        </div>' +
                             '            <div class="view-commits">' +
                             '                <button class="view-commits-btn" value="' + repoName + '">View Commits</button>' +
-                            '                <ul class="repo-commits"></ul>' +
+                            '                <button class="toggle-commits-btn" value="' + repoName + '" style="display: none;">Hide Commits</button>' +
+                            '                <ul class="repo-commits" style="display: none;"></ul>' +
                             '            </div>' +
-                            '        </div>' +
                             '    </div>' +
                             '</li>');
 
                             });
+                            
+                            
 
                             $('.view-commits-btn').on('click', function(getCommits) {
                                 searchedCompany.searchedRepoCommits = $(this).val();
                                 repoName = $(this).val();
-                                console.log(searchedCompany.searchedRepoCommits);
+                                $('.view-commits-btn[value=' + repoName +']').hide();
+                                $('.toggle-commits-btn[value=' + repoName +']').show();
 
                                 $.ajax({
                                     dataType: "json",
@@ -92,24 +115,40 @@ $(document).ready( function() {
                                         console.log(getRepoCommits);
 
                                         $.each(getRepoCommits, function(i,e) {
-                                        var commitTitle = getRepoCommits[i].commit.message;
-                                        var commitAuthor = getRepoCommits[i].commit.author.name;
-                                        var commitSha = getRepoCommits[i].sha;
+                                        var commitMessage = getRepoCommits[i].commit.message;
+                                        var commitAuthor = getRepoCommits[i].author.login;
+                                        var commitSha = getRepoCommits[i].parents[0].sha;
+                                        var commitUrl = getRepoCommits[i].parents[0].html_url;
                                         var commitCreated = getRepoCommits[i].commit.committer.date;
-                                        console.log(commitTitle);
-                                        console.log(commitAuthor);
-                                        console.log(commitHash);
-                                        console.log(commitCreated);
                                         //Only return created date
                                         let commitDate = commitCreated.slice(0, 10);
                                         let commitHash = commitSha.slice(0, 7);
+                                        let commitTitle = commitMessage.slice(0, 67);
 
                                         $('[value=' + repoName +']').next('ul').append(
-                                            '<li>' + commitTitle + ' | ' + commitAuthor + ' | ' + commitHash + ' | ' + commitDate + '</li>'
-                                        )
+                                            '<li>' +
+                                            '   <div class="commit">' +
+                                            '       <div class="commit-message">' +
+                                            '           <span>' + commitTitle +'</span>' +
+                                            '       </div>' +
+                                            '       <div class="commit-info">' +
+                                            '           <span class="commit-author">' + commitAuthor + '</span>' +
+                                            '           <span class="commit-date">' + commitDate + '</span>' +
+                                            '       </div>' +
+                                            '   </div>' +
+                                            '   <div class="commit-hash">' +
+                                            '       <a aria-label="go to this commit" target="_blank" href="' + commitUrl + '"><span>' + commitHash + '</span></a>' +
+                                            '   </div>' +
+                                            '</li>')
+                                            $('[value=' + repoName +']').next('ul').slideDown();
                                         console.log(getRepoCommits);
                                         });
                                 }});
+
+                                $('.toggle-commits-btn[value=' + repoName +']').on('click', function() {
+                                    $('[value=' + repoName +']').next('ul').slideToggle();
+                                    $(this).text($(this).text() == 'Hide Commits' ? 'View Commits' : 'Hide Commits');
+                                });
                             });
 
                         // Sort repositories by star count, biggest to smallest
@@ -118,13 +157,41 @@ $(document).ready( function() {
                             function sort_li(a, b) {
                                 return ($(b).data('position')) > ($(a).data('position')) ? 1 : -1;
                             }
+                            pagnation()
                             });
-                            $("#repositories li").show();
-
+                            function pagnation() {
+                                $('.repository-container').after('<ul id="repositories"></ul>');
+                                var rowsShown = 5;
+                                var rowsTotal = $('#repositories li').length;
+                                var numPages = rowsTotal/rowsShown;
+                                console.log(rowsTotal);
+                                for(i = 0;i < numPages;i++) {
+                                    var pageNum = i + 1;
+                                    $('#page-tabs').append('<button href="#" rel="'+i+'">'+pageNum+'</button> ');
+                                }
+                                $('#repositories li').hide();
+                                $('#repositories li').slice(0, rowsShown).show();
+                                $('#page-tabs button:first').addClass('active');
+                                $('#page-tabs button').bind('click', function(){
+                                    $('#page-tabs button').removeClass('active');
+                                    $(this).addClass('active');
+                                    var currPage = $(this).attr('rel');
+                                    var startItem = currPage * rowsShown;
+                                    var endItem = startItem + rowsShown;
+                                    $('#repositories li').css('opacity','0.0').hide().slice(startItem, endItem).
+                                            css('display','flex').animate({opacity:1}, 300);
+                                });
+                                $('#page-tabs button:first').trigger("click");
+                            }
+                            
                 },
                 error: function() {
                     // Show error if company not found
                     $('#search-failed').show();
+                    $('.search').addClass('search-error');
+                    setTimeout(function () { 
+                        $('.search').removeClass('search-error');
+                    }, 200);
                 }
             }); 
         }});
